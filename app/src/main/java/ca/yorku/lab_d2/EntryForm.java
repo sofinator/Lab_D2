7cd46b17cdf4ea938d0d2cf7f4df4d696e2d37d5
user <sdfg>
2020-09-23 22:08:59 -0400

    up

--- app/src/main/java/ca/yorku/lab_d2/EntryForm.java
+++ app/src/main/java/ca/yorku/lab_d2/EntryForm.java
@@ -3,6 +3,8 @@ package ca.yorku.lab_d2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class EntryForm extends AppCompatActivity
 {
@@ -12,5 +14,17 @@ public class EntryForm extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mortgage_layout);
+    }
+
+    public void buttonClick(View v) {
+        EditText principleView = (EditText) findViewById(R.id.principleBox);
+        String pS= principleView.getText().toString();
+        EditText amortView = (EditText) findViewById(R.id.amortBox);
+        String aS= amortView.getText().toString();
+        EditText interestView = (EditText) findViewById(R.id.interestBox);
+        String iS= principleView.getText().toString();
+
+        MortgageModel myModel = new MortgageModel(pS,aS,iS);
+        myModel.computePayment();
     }
 }
--- app/src/main/java/ca/yorku/lab_d2/MortgageModel.java
+++ app/src/main/java/ca/yorku/lab_d2/MortgageModel.java
@@ -2,10 +2,14 @@ package ca.yorku.lab_d2;
 
 public class MortgageModel
 {
+    double r;
+    double P;
+    double n;
+
     public MortgageModel(String p, String a, String i) {
-        double P = Double.parseDouble(p);
-        double r = Double.parseDouble(i);
-        double n = Double.parseDouble(a);
+        r = Double.parseDouble(i)/100/12;
+        P = Double.parseDouble(p);
+        n = Double.parseDouble(a)*12;
     }
     public String computePayment() {
         double payment = (r * P) / (1 - Math.pow((1 + r),-n));
